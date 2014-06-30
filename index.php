2d99d5f3799a2747f2d19f2a21f26cc80bb01498
meza <meza@meza.hu>
2014-06-30 15:49:35 +0200

    Added meta description #50

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
 	<title>Várda-Véd Kft. | Tűzvédelem, Munkavédelem</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1.0, user-scalable=no">
+    <meta name="description" content="Összpontosítson cégének azon feladataira, amelyekből bevétele, profitja származik! A
+					munkavédelmi törvények értelmezését, nyomon követését és a feladatok ellátását bízza a
+					Várda-Véd Kft. szakembereire!">
+
     <link rel="icon" type="image/x-icon" href="/assets/images/varda-ved.ico" />
 	<link href="assets/stylesheets/main.css" rel="stylesheet">
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:600,400,300' rel='stylesheet'
